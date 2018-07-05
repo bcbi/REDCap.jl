ce9680cf1625add3cebf8dad780cf004975a84ed
Cory <Falcongray17@gmail.com>
2018-07-05 13:57:06 -0400

    file handling and testing work

--- test/fieldCreation.jl
+++ test/fieldCreation.jl
@@ -155,34 +155,3 @@ function generate_file()
 	=#
 
 end
-
-
-
-#=
-#Field Names-
-[{"original_field_name":"record_id","choice_value":"","export_field_name":"record_id"},{"original_field_name":"first_name","choice_value":"","export_field_name":"first_name"},{"original_field_name":"last_name","choice_value":"","export_field_name":"last_name"},{"original_field_name":"address","choice_value":"","export_field_name":"address"},{"original_field_name":"telephone","choice_value":"","export_field_name":"telephone"},{"original_field_name":"email","choice_value":"","export_field_name":"email"},{"original_field_name":"dob","choice_value":"","export_field_name":"dob"},{"original_field_name":"age","choice_value":"","export_field_name":"age"},{"original_field_name":"ethnicity","choice_value":"","export_field_name":"ethnicity"},{"original_field_name":"race","choice_value":"","export_field_name":"race"},{"original_field_name":"sex","choice_value":"","export_field_name":"sex"},{"original_field_name":"height","choice_value":"","export_field_name":"height"},{"original_field_name":"weigh
-
-#Instruments
-[{"instrument_name":"demographics","instrument_label":"Basic Demography Form"}]"""
-
-#Metadata
-[{"field_name":"record_id","form_name":"demographics","section_header":"","field_type":"text","field_label":"Study ID","select_choices_or_calculations":"","field_note":"","text_validation_type_or_show_slider_number":"","text_validation_min":"","text_validation_max":"","identifier":"","branching_logic":"","required_field":"","custom_alignment":"","question_number":"","matrix_group_name":"","matrix_ranking":"","field_annotation":""},
-{"field_name":"first_name","form_name":"demographics","section_header":"Contact Information","field_type":"text","field_label":"First Name","select_choices_or_calculations":"","field_note":"","text_validation_type_or_show_slider_number":"","text_validation_min":"","text_validation_max":"","identifier":"y","branching_logic":"","required_field":"","custom_alignment":"","question_number":"","matrix_group_name":"","matrix_ranking":"","field_annotation":""},
-{"field_name":"last_name","form_name":"demographics","section_header":"","field_type":"text","field_label"
-
-#Project Info
-{"project_id":"18","project_title":"Test-proj","creation_time":"2018-06-11 15:08:22","production_time":"","in_production":"0","project_language":"English","purpose":"0","purpose_other":"","project_notes":"Cory CIS","custom_record_label":"","secondary_unique_field":"","is_longitudinal":0,"surveys_enabled":"1","scheduling_enabled":"0","record_autonumbering_enabled":"1","randomization_enabled":"0","ddp_enabled":"0","project_irb_number":"","project_grant_number":"","project_pi_firstname":"","project_pi_lastname":"","display_today_now_button":"1","has_repeating_instruments_or_events":0}"""
-
-#User
-[{"username":"cory_cothrum@brown.edu","email":"cory_cothrum@brown.edu","firstname":"","lastname":"","expiration":"","data_access_group":"","data_access_group_id":"","design":"1","user_rights":"1","data_access_groups":"1","data_export":"1","reports":"1","stats_and_charts":"1","manage_survey_participants":"1","calendar":"1","data_import_tool":"1","data_comparison_tool":"1","logging":"1","file_repository":"1","data_quality_create":"1","data_quality_execute":"1","api_export":"1","api_import":"1","mobile_app":"1","mobile_app_download_data":"1","record_create":"1","record_rename":"1","record_delete":"1","lock_records_all_forms":"0","lock_records":"0","lock_records_customization":"0","forms":{"demographics":"1"}},{"username":"fernando_gelin@brown.edu","email":"fernando_gelin@brown.edu","firstname":"","lastname":"","expiration":"","data_access_group":"","data_access_group_id":"","design":"1","user_rights":"1","data_access_groups":"1","data_export":"1","reports":"1","stats_and_charts":"1","mana
-
-#NO ARMS - not long
-
-
-#NO EVENTS - not long
-
-#Record
-[{"record_id":"1","first_name":"John","last_name":"Smith","address":"123 Fake St. Providence RI 02895","telephone":"(401) 888-9956","email":"JohnSmith78@gmail.com","dob":"1978-07-08","age":"39","ethnicity":"2","race":"4","sex":"1","height":"172.96","weight":"80","bmi":"26.7","comments":"","demographics_complete":"2"},{"record_id":"2","first_name":"Cory","last_name":"Cothrum","address":"335 Harrington Ave. Warwick RI 02888","telephone":"(401) 338-8369","email":"Cory_Cothrum@brown.edu","dob":"1992-12-14","age":"25","ethnicity":"1","race":"4","sex":"1","height":"187.96","weight":"99","bmi":"28","comments":"self","demographics_complete":"1"},{"record_id":"3","first_name":"colby","last_name":"Smith","address":"123 Fake St. Providence RI 02895","telephone":"(401) 888-9956","email":"JohnSmith78@gmail.com","dob":"1978-07-08","age":"39","ethnicity":"2","race":"4","sex":"1","height":"172.96","weight":"80","bmi":"26.7","comments":"","demographics_complete":"2"},{"record_id":"4","first_name":"mark
-
-
-=#
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,28 +1,23 @@
-using REDCap
-
 @static if VERSION < v"0.7.0-DEV.2005"
     using Base.Test
 else
     using Test
 end
 
-# write your own tests here
-#@test 1 == 2
+using REDCap
 
-
+const REDCap_API = get(ENV, "REDCAP_API", "")
+const REDCap_URL = get(ENV, "REDCAP_URL", "")
 
 # -=: Test: Functionality :=- #
 @testset "Functionality" begin
 	#setup for any needed vars
-	config = SuperConfig("url", "skey")
+	config = Config(REDCap_URL, REDCap_API)
 	
 	r = HTTP.get(config.url)
 	@test r.status == 200
 
-	@test 1 == 1 #will fail if this throws an error- write tests for Bool vals
-
-	#=
-	#	TESTING:
+	#=	TESTING:
 	# needs to be able to manipulate a project, and verify what comes back. Create project in users environment? Dedicated test env?
 	# Must: Create, Import, Export, make sure what you put in is what you got out, and delete everything properly.
 	# Create: make a project from scratch using stock settings- can just be a premade dict.
@@ -34,23 +29,100 @@ end
 	# Delete: Delete everything from the project piece by piece, and finally the project-
 	# 	Verify everything, and try to export from it again.
 	# 
-	# 
 	=#
-	modules = [:(run_test(7)),
-			   :(run_test(1)),
-			   :(run_test(2)),]
-	passed = true
-	for i in modules
-		@test eval(i)==true
+	
+	#Creating
+	#project_config = create_project(super_config, "Test Project", 1; purpose_other="Testing REDCap.jl Functionality", project_notes="This is not an actual REDCap Database.", 
+	#					is_longitudinal=1, surveys_enabled=1, record_autonumbering_enabled=1)
+
+
+	#Importing- 
+	#Make ~10 records, verify matches.
+
+	id = generate_next_record_id(config)
+	records = Array{Any}(10)
+	for i in 1:length(records)
+		records[i] = record_generator(config, id)
+		id+=1
 	end
+	response = import_records(config, records)
+	println(response); println(get(response, "count", "x"))
+
+	@test response["count"] == length(records)
 
 
 
 
-
-
-
-
-
+	#Exporting
+	function test_export_method1(config::Config)
+		#=
+		println("TEST 1 - Press X to JSON")
+		println(export_field_names(config))
+		println(export_instruments(config))
+		println(export_metadata(config))
+		println(export_project_information(config))
+		println(export_user(config))
+		println(export_version(config))
+		#println(export_arms(config))
+		#println(export_events(config))
+		println(export_pdf(config))
+		println(export_project(config))
+		println(export_records(config))
+		println(export_survey_link(config, 1, "demographics", "1", 1))
+		#println(export_survey_queue_link(config, "1"))
+		#println(export_survey_return_code(config, "1", "demographics", "1", "1"))
+		#println(export_instrument_event_mappings(config))
+		println(export_survey_participant_list(config, "demographics", "1"))
+		#println(export_file(config))
+		#println(export_reports(config))
+		=#
+		#=
+		println("TEST 2 - XML")
+		println(export_field_names(config, format="xml"))
+		println(export_instruments(config, format="xml"))
+		println(export_metadata(config, format="xml"))
+		println(export_project_information(config, format="xml", returnFormat="xml"))
+		println(export_user(config, format="xml", returnFormat="xml"))
+		println(export_version(config, format="xml", returnFormat="xml"))
+		#println(export_arms(config))
+		#println(export_events(config))
+		println(export_pdf(config))
+		println(export_project(config))
+		println(export_records(config, format="xml"))
+		println(export_survey_link(config, 1, "demographics", "1", 1))
+		#println(export_survey_queue_link(config, "1"))
+		#println(export_survey_return_code(config, "1", "demographics", "1", "1"))
+		#println(export_instrument_event_mappings(config))
+		println(export_survey_participant_list(config, "demographics", "1", format="xml", returnFormat="xml"))
+		#println(export_file(config))
+		#println(export_reports(config))
+		=#
+		
+		println("TEST 3 - CSV")
+		#=
+		println(export_field_names(config, format="csv"))
+		println(export_instruments(config, format="csv"))
+		println(export_metadata(config, format="csv"))
+		println(export_project_information(config, format="csv", returnFormat="csv"))
+		println(export_user(config, format="csv", returnFormat="csv"))
+		println(export_version(config, format="csv", returnFormat="csv"))
+		#println(export_arms(config))
+		#println(export_events(config))
+		println(export_pdf(config))
+		println(export_project(config))
+		println(export_records(config, format="csv"))
+		=#
+		println(export_records(config, format="csv"))
+		println(typeof(export_records(config, format="csv")))
+		println("Saving"); CSV.write("~/gitHub/REDCap.jl/csvout.txt", export_records(config, format="csv"))
+		println(export_survey_link(config, 1, "demographics", "1", 1))
+		#println(export_survey_queue_link(config, "1"))
+		#println(export_survey_return_code(config, "1", "demographics", "1", "1"))
+		#println(export_instrument_event_mappings(config))
+		println(export_survey_participant_list(config, "demographics", "1", format="csv", returnFormat="csv"))
+		#println(export_file(config))
+		#println(export_reports(config))
+		
+	end
 
 end
