075b3669758704df90b15e0618e5904053ed3c7e
FLCN17 <Falcongray17@gmail.com>
2018-08-28 15:08:36 -0400

    finishing tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -36,22 +36,7 @@ else
 		else
 			@error("Cannot find REDCap URL in environment.")	
 		end	
-
-		#=	TESTING:
-		# needs to be able to manipulate a project, and verify what comes back. Create project in users environment? Dedicated test env?
-		# Must: Create, Import, Export, make sure what you put in is what you got out, and delete everything properly.
-		# Create: make a project from scratch using stock settings
-		# Import: Make (and save) a bunch of generic constructions for fields, metadata, records, everything that can be imported- 
-		# 	After, lock the project/make it as 'out of development' and try to re-import everything that-
-		#	should be blocked once out of dev in REDCap.
-		# Export: grab everything from the project and verify it matches the generated data perfectly- 
-		#	also check any files and the pdf and xml of project.
-		# Delete: Delete everything from the project piece by piece, and finally the project-
-		# 	Verify everything, and try to export from it again.
-		#
-		# Have a file that contains ~40 example records, and all testing data needed? Not include generators? Space vs. complexity
-		# 
-		=#
+		
 		if full_test
 			println("Creating Project")
 			#Creating
