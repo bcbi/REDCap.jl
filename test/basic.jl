031042e3f8668b723918308ccb969c8cbc2000f9
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-08-10 00:12:55 -0400

    Test all user-facing functions

--- test/basic.jl
+++ test/basic.jl
@@ -1,37 +1,55 @@
 using REDCap
+using Test
 
-#REDCap.get_redcap_superuser_config();
-#REDCap.get_redcap_user_config();
+test_sets = Dict(
+	"Delete" => [
+		:(delete_arms(arms)),
+		:(delete_events(events)),
+		:(delete_file(record, field, event)),
+		:(delete_records(records)),
+	],
 
-function_calls = [
-	:(export_field_names()),
-	:(export_instruments()),
-	:(export_metadata()),
-	:(export_project_information()),
-	:(export_users()),
-	:(export_version()),
-	:(export_arms()),
-	:(export_events()),
-	:(export_pdf("./X.pdf")),
-	:(export_project()),
-	:(export_records()),
-	:(export_survey_queue_link(record::String)),
-	:(export_survey_return_code(record::String, instrument::String, event::String)),
-	:(export_instrument_event_mappings()),
-	:(export_survey_participant_list(instrument::String, event::String)),
-	:(export_file(record::String, field::String, event::String)),
-	:(export_report(report_id)),
-	:(export_survey_link(record::String, instrument::String, event::String)),
-       ]
+	"Import" => [
+		:(import_project_information(data)),
+		:(import_metadata(data)),
+		:(import_users(data)),
+		:(import_arms(data)),
+		:(import_events(data)),
+		:(import_records(data)),
+		:(import_instrument_event_mappings(data)),
+		:(import_file(record, field, event, file)),
+		:(create_project(project_title, purpose)),
+	],
 
-@testset "Export" begin
-	for fc in function_calls
-		@testset "$fc" begin
-			eval(fc)
-			@test true
+	"Export" => [
+		:(export_field_names()),
+		:(export_instruments()),
+		:(export_metadata()),
+		:(export_project_information()),
+		:(export_users()),
+		:(export_version()),
+		:(export_arms()),
+		:(export_events()),
+		:(export_pdf("./X.pdf")),
+		:(export_project()),
+		:(export_records()),
+		:(export_survey_queue_link(record::String)),
+		:(export_survey_return_code(record::String, instrument::String, event::String)),
+		:(export_instrument_event_mappings()),
+		:(export_survey_participant_list(instrument::String, event::String)),
+		:(export_file(record::String, field::String, event::String)),
+		:(export_report(report_id)),
+		:(export_survey_link(record::String, instrument::String, event::String)),
+	       ],
+)
+
+for test_set_name in keys(test_sets)
+	@testset "$test_set_name" begin
+		for function_call in test_sets[test_set_name]
+			@testset "$function_call" begin
+					eval(function_call)
+				@test true
+			end
 		end
 	end
 end
-nothing
-
-
