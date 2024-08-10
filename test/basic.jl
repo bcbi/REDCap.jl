c85ed9164ca16319678fc194858f7940eb8e9f17
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-08-09 20:21:43 -0400

    Add basic function test

---
+++ test/basic.jl
@@ -0,0 +1,24 @@
+using REDCap
+
+REDCap.get_redcap_superuser_config();
+REDCap.get_redcap_user_config();
+
+export_field_names();
+export_instruments();
+export_metadata();
+export_project_information();
+export_users();
+export_version();
+#CLASSIC: export_arms();
+#CLASSIC: export_events();
+#export_pdf(file_loc::String);
+export_project();
+export_records();
+#export_survey_queue_link(record::String);
+#export_survey_return_code(record::String, instrument::String, event::String);
+#CLASSIC: export_instrument_event_mappings();
+#export_survey_participant_list(instrument::String, event::String);
+#export_file(config::REDCap.Config, record::String, field::String, event::String);
+#export_report(report_id);
+#export_survey_link(record::String, instrument::String, event::String);
+
