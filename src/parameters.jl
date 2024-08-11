af07f92dbd31de097f413f17ee7f06153fb83357
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-08-11 14:36:15 -0400

    Add content and format parameter macros

--- src/parameters.jl
+++ src/parameters.jl
@@ -1,6 +1,36 @@
 macro action(arg)
-	if arg in ["delete","export","import"]
+	if arg in ["rename", "delete","export","import", "list", "switch"]
 		kwarg = Expr(:kw, :action, arg)
+		return esc( :( $(kwarg) ) )
+	else
+		error("Unsupported REDCap API parameter")
+		throw(ArgumentError)
+	end
+end
+
+macro content(arg)
+	if arg in ["arm","dag","userDagMapping", "event", "exportFieldNames", "file", "fileRepository", "instrument", "pdf", "formEventMapping", "log", "metadata", "project", "projectSettings", "project_xml", "record", "generateNextRecordName", "repeatingFormsEvents", "report", "version", "surveyLink", "participantList", "surveyQueueLink", "surveyReturnCode", "user", "userRole", "userRoleMapping"]
+		kwarg = Expr(:kw, :content, arg)
+		return esc( :( $(kwarg) ) )
+	else
+		error("Unsupported REDCap API parameter")
+		throw(ArgumentError)
+	end
+end
+
+macro format(arg)
+	if arg in ["csv","json","xml", "odm"]
+		kwarg = Expr(:kw, :format, arg)
+		return esc( :( $(kwarg) ) )
+	else
+		error("Unsupported REDCap API parameter")
+		throw(ArgumentError)
+	end
+end
+
+macro returnFormat(arg)
+	if arg in ["csv","json","xml"]
+		kwarg = Expr(:kw, :returnFormat, arg)
 		return esc( :( $(kwarg) ) )
 	else
 		error("Unsupported REDCap API parameter")
