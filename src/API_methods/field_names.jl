c0e96f11f4a821a41b6c5e41613079008fa9a08b
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-08-11 18:41:51 -0400

    Remove old method files

---
+++ src/API_methods/field_names.jl
@@ -0,0 +1,12 @@
+export export_list_of_export_field_names
+
+function export_list_of_export_field_names(;format="xml",field=nothing,returnFormat=nothing)
+	redcap_api(;
+		url=get_valid_url(),
+		token=get_valid_token(),
+		content="exportFieldNames",
+		format=get_valid_format(format),
+		field=field, #TODO: This could be a single entry or an array
+		returnFormat=isnothing(returnFormat) ? format : "xml",
+	)
+end
