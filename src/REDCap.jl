098a95fa3b5703fe75a334b1910be0a6337c593f
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-08-11 00:02:10 -0400

    Rename files for consistency

--- src/REDCap.jl
+++ src/REDCap.jl
@@ -3,10 +3,10 @@ module REDCap
 using HTTP
 using JSON
 
-include("Utils.jl")
-include("API_Methods/arms.jl")
-include("API_Methods/data_access_groups.jl")
-include("API_Methods/metadata.jl")
-include("API_Methods/REDCap.jl")
+include("utils.jl")
+include("API_methods/arms.jl")
+include("API_methods/data_access_groups.jl")
+include("API_methods/metadata.jl")
+include("API_methods/REDCap.jl")
 
 end
