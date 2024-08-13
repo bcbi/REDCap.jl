065ed422847965e6fef96ebc66cfee9a15df6d50
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-08-12 21:04:08 -0400

    Remove ignored parameter

--- src/api_methods/redcap.jl
+++ src/api_methods/redcap.jl
@@ -1,10 +1,9 @@
 export export_version
 
-function export_version(;format="xml")
+function export_version()
 	REDCap.request(;
 		url=get_valid_url(),
 		token=get_valid_token(),
 		content="version",
-		format=assert_valid_format(format),
 	)
 end
