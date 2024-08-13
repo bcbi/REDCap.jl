e9931273b2ae6e62a692b7b104c51d14f70154ce
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-08-12 20:24:39 -0400

    Bug fix

--- src/api_methods/metadata.jl
+++ src/api_methods/metadata.jl
@@ -1,6 +1,6 @@
 export export_metadata,	import_metadata
 
-function export_metadata(;format="xml",fields=nothing,forms=nothing)
+function export_metadata(;format="xml",fields=nothing,forms=nothing,returnFormat=nothing)
 	REDCap.request(
 		url=get_valid_url(),
 		token=get_valid_token(),
@@ -8,7 +8,7 @@ function export_metadata(;format="xml",fields=nothing,forms=nothing)
 		format=assert_valid_format(format),
 		fields=fields,
 		forms=forms,
-		returnFormat=assert_valid_format(returnFormat),
+		returnFormat=isnothing(returnFormat) ? format : "xml",
 	)
 end
 
