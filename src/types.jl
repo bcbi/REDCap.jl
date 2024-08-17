1b27b854dfc141964f505890fdcfb8009565d067
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-08-17 14:42:27 -0400

    Improve types

--- src/types.jl
+++ src/types.jl
@@ -8,7 +8,8 @@ end
 REDCap_token(x::REDCap_token) = x
 Base.print(x::REDCap_token) = print(x.id)
 URIs.escapeuri(key::String, value::REDCap.REDCap_token) = URIs.escapeuri(key, value.id)
-#Base.show(io::IO, token::REDCap_token) = print(io, token.id)
+Base.show(io::IO, token::REDCap_token) = print(io, token.id)
+String(x::REDCap_token) = x.id
 #macro redcap_token_str(t); REDCap_token(t) ; end #TODO: Is this macro even helpful in any way?
 
 #TODO: add type assertions for all the kwargs somewhere?
@@ -24,6 +25,7 @@ end
 REDCap_token(x::REDCap_super_token) = x #TODO: improve name - this could be confusing
 Base.print(x::REDCap_super_token) = print(x.id) #TODO: just define once on a supertype?
 URIs.escapeuri(key::String, value::REDCap.REDCap_super_token) = URIs.escapeuri(key, value.id)
+String(x::REDCap_super_token) = x.id
 
 #=
 struct REDCap_url
@@ -47,18 +49,17 @@ struct REDCap_format
        is_valid(format) = format ∈ [:csv,:json,:xml]
        end
 Base.print(x::REDCap_format) = print(x.id)
+String(x::REDCap_format) = x.id
 
 redcap_action_parameter = Union{Symbol, Nothing}
 redcap_content_parameter = Union{Symbol, Nothing}
-
 redcap_data_parameter = Union{Dict, String, Nothing} #TODO: this can be nothing in REDCap.request(), but must be mandatory wherever it is a method parameter. Otherwise, it changes the method behavior, which would contradict the guarantee of the function name
 redcap_filterLogic_parameter = Union{String, Nothing}
 redcap_odm_parameter = Union{String, Nothing}
 
-#TODO: Deprecate these, since they must always have a value?
-# I'm leaning towards keeping them, just in case.
 redcap_super_token_parameter = Union{REDCap_super_token, String}
-redcap_token_parameter = Union{REDCap_token, REDCap_super_token, String} # Fix this name
+redcap_token_parameter = Union{REDCap_token, REDCap_super_token, String}
+redcap_either_token = Union{redcap_token_parameter, redcap_super_token_parameter}
 redcap_url_parameter = Union{REDCap_url,String}
 
 redcap_array = Union{Array, Nothing}
--- src/utils.jl
+++ src/utils.jl
@@ -1,15 +1,14 @@
 function request(;
 	dry_run=false,
-	#url::redcap_url_parameter,
-	#token::redcap_token_parameter,
-	#content::redcap_content_parameter,
-	url,token,content,
+	url::redcap_url_parameter,
+	token::redcap_either_token,
+	content::redcap_content_parameter,
 	kwargs...)
 
 	html_request_body = assemble_html_body(;kwargs...)
 	html_request_body["x-forwarded-proto"] = "https"
 	html_request_body["token"] = REDCap_token(token)
-	html_request_body["content"] = content
+	html_request_body["content"] = "$content"
 
 	return dry_run ? html_request_body : HTTP.post(
 		URI(url);
@@ -23,7 +22,7 @@ function request(;
 end
 
 function assemble_html_body(;kwargs...)
-	body = Dict()
+	body = Dict{String, String}()
 	if !isempty(kwargs)
 		for (parameter,value) in kwargs
 			if !isnothing(value)
@@ -32,7 +31,7 @@ function assemble_html_body(;kwargs...)
 						body["$parameter[$(i-1)]"] = "$item"
 					end
 				else
-					body[String(parameter)] = "$value"
+					body["$parameter"] = "$value"
 				end
 			end
 		end
@@ -51,7 +50,7 @@ function assemble_data_parameter(;kwargs...)
 						data["$attribute[$(i-1)]"] = "$item"
 					end
 				else
-					data[String(attribute)] = "$value"
+					data["attribute"] = "$value"
 				end
 			end
 		end
