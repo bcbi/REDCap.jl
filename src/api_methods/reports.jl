93199cdc0d125619d18705728b3149c3a1e919a3
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-08-21 18:06:45 -0400

    Add report method

--- src/api_methods/reports.jl
+++ src/api_methods/reports.jl
@@ -1,10 +1,20 @@
 export export_reports
 
-function export_reports(;report_id=nothing,format=:xml,returnFormat=nothing,rawOrLabel="raw",rawOrLabelHeaders="raw",exportCheckboxLabel=false,csvDelimiter=',',decimalCharacter=".")
+function export_reports(;
+	report_id=nothing,
+	format=nothing,
+	returnFormat=nothing,
+	rawOrLabel=nothing,
+	rawOrLabelHeaders=nothing,
+	exportCheckboxLabel=nothing,
+	csvDelimiter=nothing,
+	decimalCharacter=nothing,
+	)
+
 	REDCap.request(
-		       content=REDCap_content(:report),
+		content=REDCap_content(:report),
 		report_id=report_id,
-		format=REDCap_format(format), # format can include odm
+		format=REDCap_format(format), #TODO: format can include odm
 		returnFormat=REDCap_format(returnFormat),
 		rawOrLabel=rawOrLabel,
 		rawOrLabelHeaders=rawOrLabelHeaders,
