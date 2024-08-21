705bc095f2c31b097d73a67e007e5d1ce20eb42c
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-08-21 18:21:40 -0400

    Update survey methods

--- src/REDCap.jl
+++ src/REDCap.jl
@@ -21,7 +21,7 @@ include("api_methods/records.jl")
 include("api_methods/redcap.jl")
 include("api_methods/repeating_instruments_and_events.jl")
 include("api_methods/reports.jl")
-#include("api_methods/surveys.jl")
+include("api_methods/surveys.jl")
 include("api_methods/users.jl")
 include("api_methods/user_roles.jl")
 
--- src/api_methods/surveys.jl
+++ src/api_methods/surveys.jl
@@ -3,8 +3,19 @@ export_survey_participants,
 export_survey_queue_link,
 export_survey_return_code
 
-function export_survey_link(;record=nothing,instrument=nothing,event=nothing,repeat_instance=nothing,returnFormat=:xml)
+function export_survey_link(;
+	url::redcap_url_parameter=get_url(),
+	token::redcap_token_parameter=get_token(),	
+	returnFormat::redcap_returnFormat_parameter=nothing,
+	record=nothing,
+	instrument=nothing,
+	event=nothing,
+	repeat_instance=nothing,
+	)
+
 	REDCap.request(
+		url=URI(url),
+		token=REDCap_token(token),
 		       content=REDCap_content(:surveyLink),
 		record=record,
 		instrument=instrument,
@@ -14,8 +25,19 @@ function export_survey_link(;record=nothing,instrument=nothing,event=nothing,rep
 	)
 end
 
-function export_survey_participants(;instrument=nothing,event=nothing,repeat_instance=nothing,format=:xml,returnFormat=nothing)
+function export_survey_participants(;
+	url::redcap_url_parameter=get_url(),
+	token::redcap_token_parameter=get_token(),	
+	format::redcap_format_parameter=nothing,
+	returnFormat::redcap_returnFormat_parameter=nothing,
+	instrument=nothing,
+	event=nothing,
+	repeat_instance=nothing,
+	)
+
 	REDCap.request(
+		url=URI(url),
+		token=REDCap_token(token),
 		       content=REDCap_content(:participantList),
 		instrument=instrument,
 		event=event,
@@ -27,14 +49,26 @@ end
 
 function export_survey_queue_link(;record=nothing,returnFormat=:xml)
 	REDCap.request(
-		       content=REDCap_content(:surveyQueueLink),
+		url=URI(url),
+		token=REDCap_token(token),
+	       content=REDCap_content(:surveyQueueLink),
 		record=record,
 		returnFormat=REDCap_format(returnFormat),
 	)
 end
 
-function export_survey_return_code(;record=nothing,instrument=nothing,event=nothing,repeat_instance=nothing,returnFormat=:xml)
+function export_survey_return_code(;
+	url::redcap_url_parameter=get_url(),
+	token::redcap_token_parameter=get_token(),	
+	returnFormat::redcap_returnFormat_parameter=nothing,
+	record=nothing,
+	instrument=nothing,
+	event=nothing,
+	repeat_instance=nothing,
+)
 	REDCap.request(
+		url=URI(url),
+		token=REDCap_token(token),
 		       content=REDCap_content(:surveyLink),
 		record=record,
 		instrument=instrument,
