e60f493ba75ba8fa6306998bc3369c91408835b2
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-08-21 12:29:46 -0400

    Add repeating instrument methods

--- src/api_methods/repeating_instruments_and_events.jl
+++ src/api_methods/repeating_instruments_and_events.jl
@@ -1,16 +1,34 @@
 export export_repeating_instruments_and_events,
 import_repeating_instruments_and_events
 
-function export_repeating_instruments_and_events(;format=:xml)
+function export_repeating_instruments_and_events(;
+	url::redcap_url_parameter=get_url(),
+	token::redcap_super_token_parameter=get_token(),	
+	format::redcap_format_parameter=nothing,
+	)
+
 	REDCap.request(
-		       content=REDCap_content(:repeatingFormsEvents),
+		url=URI(url),
+		token=REDCap_token(token),
+		content=REDCap_content(:repeatingFormsEvents),
 		format=REDCap_format(format),
 	)
 end
 
-function import_repeating_instruments_and_events(;format=:xml,data=nothing,returnFormat=nothing)
+function import_repeating_instruments_and_events(;
+	url::redcap_url_parameter=get_url(),
+	token::redcap_super_token_parameter=get_token(),	
+	format::redcap_format_parameter=nothing,
+	returnFormat::redcap_returnFormat_parameter=nothing,
+	data::redcap_data_parameter, #TODO: the docs mention a super API token may be used here...?
+	#TODO: data parameter is essential to distringuish from an export
+	#TODO: does is make sense to define one method in terms of the other?
+	)
+
 	REDCap.request(
-		       content=REDCap_content(:repeatingFormsEvents),
+		content=REDCap_content(:repeatingFormsEvents),
+		url=URI(url),
+		token=REDCap_token(token),
 		format=REDCap_format(format),
 		returnFormat=REDCap_format(returnFormat),
 	)
