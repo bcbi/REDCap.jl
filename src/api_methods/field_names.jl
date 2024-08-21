55cc17fd840a19f1c32eb5f07feb6ac3c689b253
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-08-20 23:45:04 -0400

    Add field name method

--- src/api_methods/field_names.jl
+++ src/api_methods/field_names.jl
@@ -1,7 +1,16 @@
 export export_list_of_export_field_names
 
-function export_list_of_export_field_names(;format=:xml,field=nothing,returnFormat=nothing)
+function export_list_of_export_field_names(;
+	url::redcap_url_parameter=get_url(),
+	token::redcap_token_parameter=get_token(),	
+	format::redcap_format_parameter,
+	returnFormat::redcap_returnFormat_parameter=nothing,
+	field=nothing
+	)
+
 	REDCap.request(;
+		url=URI(url),
+		token=REDCap_token(token),
 		content=:exportFieldNames,
 		format=REDCap_format(format),
 		field=field, #TODO: This could be a single entry or an array
