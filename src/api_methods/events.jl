6b7697435c3944b0a82d65015f1abefc75f453a2
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-08-22 18:58:57 -0400

    Update event methods

--- src/api_methods/events.jl
+++ src/api_methods/events.jl
@@ -2,31 +2,57 @@ export delete_events,
 	import_events,
 	export_events
 
-function delete_events(;events=nothing)
+function delete_events(;
+	url::redcap_url_parameter=get_url(),
+	token::redcap_token_parameter=get_token(),	
+		events=nothing,
+		)
+
 	REDCap.request(;
+		url=URI(url),
+		token=REDCap_token(token),
 		       content=REDCap_content(:event),
 		       action=REDCap_action(:import),
 		events=events,
 	)
 end
 
-function export_events(;format=:xml,arms=nothing,returnFormat=nothing)
+function export_events(;
+	url::redcap_url_parameter=get_url(),
+	token::redcap_token_parameter=get_token(),	
+	format::redcap_format_parameter=nothing,
+	returnFormat::redcap_returnFormat_parameter=nothing,
+		arms=nothing,
+		)
+
 	REDCap.request(;
-		       content=REDCap_content(:event),
+		url=URI(url),
+		token=REDCap_token(token),
 		format=REDCap_format(format),
-		arms=arms,
 		returnFormat=REDCap_format(returnFormat),
+		       content=REDCap_content(:event),
+		arms=arms,
 	)
 end
 
-function import_events(;format=:xml,data=nothing,override=0,returnFormat=nothing)
+function import_events(;
+	url::redcap_url_parameter=get_url(),
+	token::redcap_token_parameter=get_token(),	
+	format::redcap_format_parameter=nothing,
+	returnFormat::redcap_returnFormat_parameter=nothing,
+		data=nothing,
+		override=0,
+		)
+
 	REDCap.request(;
+		url=URI(url),
+		token=REDCap_token(token),
+		format=REDCap_format(format),
+		returnFormat=REDCap_format(returnFormat),
 		       content=REDCap_content(:event),
 		       action=REDCap_action(:import),
 		override=override,
-		format=REDCap_format(format),
 		data=data,
-		returnFormat=REDCap_format(returnFormat),
 	)
 end
 
