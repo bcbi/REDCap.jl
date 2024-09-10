226d4f1232e4af7c633df26c011be7a30be8d1fe
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-09-10 13:40:32 -0400

    Add export_field_names

--- src/api_methods/field_names.jl
+++ src/api_methods/field_names.jl
@@ -1,3 +1,5 @@
+#TODO: retire this old function name?
+##TODO: what about export_fields?
 function export_list_of_export_field_names(; url=get_url(), token=get_token(), format=nothing, returnFormat=nothing, field=nothing)
 #TODO: field could be a single entry or an array
 	REDCap.request(;
@@ -5,3 +7,4 @@ function export_list_of_export_field_names(; url=get_url(), token=get_token(), f
 		kwargs = (; token=REDCap_token(token), content=:exportFieldNames, format=REDCap_format(format), field, returnFormat=REDCap_format(returnFormat),),
 	)
 end
+export_field_names = export_list_of_export_field_names
