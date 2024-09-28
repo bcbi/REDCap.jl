a12bfdc36a949a366710551acee157aae5add17d
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-09-28 19:35:57 -0400

    Add docstrings

--- src/api_methods/field_names.jl
+++ src/api_methods/field_names.jl
@@ -1,8 +1,18 @@
 #TODO: retire this old function name?
 ##TODO: what about export_fields?
+"""
+	function export_list_of_export_field_names(; url=get_url(), token=get_token(), format=nothing, field=nothing)
+
+Export Events from a longitudinal REDCap project
+
+# Named arguments
+- `url`: (read from `ENV["REDCAP_API_URL"]` by default)
+- `token`: an API token specific to the REDCap project and username (read from `ENV["REDCAP_API_TOKEN"]` by default)
+- `format`: the desired output format: `:csv`, `:json`, or `:xml` (default)
+- `field`: field variable names (can be scalar or vector). By default, all fields are returned.
+
+"""
 function export_list_of_export_field_names(; url=get_url(), token=get_token(), format=nothing, field=nothing)
-#TODO: field could be a single entry or an array
-##TODO: returnFormat has no effect, which I think is generally the case for export functions
 	REDCap.request(;
 		url=REDCap_url(url),
 		kwargs = (; token=REDCap_token(token), content=:exportFieldNames, format=REDCap_format(format), field, ),
