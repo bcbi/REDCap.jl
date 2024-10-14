087c799f08530725284ae0961e43a6d33f7b3f9b
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-10-14 18:31:41 -0400

    Add any missing docstrings

--- src/api_methods/records.jl
+++ src/api_methods/records.jl
@@ -57,6 +57,7 @@ Export records from a REDCap project
 
 """
 function export_records(; url=get_url(), token=get_token(), format=nothing, type=nothing, records=nothing, fields=nothing, forms=nothing, events=nothing, rawOrLabel=nothing, rawOrLabelHeaders=nothing, exportCheckboxLabel=nothing, returnFormat=nothing, exportSurveyFields=nothing, exportDataAccessGroups=nothing, filterLogic=nothing, dateRangeBegin=nothing, dateRangeEnd=nothing, csvDelimiter=nothing, decimalCharacter=nothing, exportBlankForGrayFormStatus=nothing)
+	#TODO: is returnFormat accepted? either remove, or add to docstring
 	REDCap.request(
 		url=REDCap_url(url),
 		kwargs = (; token=REDCap_token(token), content=:record, format=REDCap_format(format), type, records, fields, forms, events, rawOrLabel, rawOrLabelHeaders, exportCheckboxLabel, returnFormat=REDCap_format(returnFormat), exportSurveyFields, exportDataAccessGroups, filterLogic, dateRangeBegin, dateRangeEnd, csvDelimiter, decimalCharacter, exportBlankForGrayFormStatus,),
@@ -101,8 +102,8 @@ Import records to a REDCap project
 - `returnContent`: sets the return value (`:count` (default, `:lds`, or `:auto_lds`)
 
 """
-#TODO: this format parameter allows odm, unlike most other format args
 function import_records(; url=get_url(), token=get_token(), format=nothing, returnFormat=nothing, type=nothing, overwriteBehavior=nothing, forceAutoNumber=nothing, backgroundProcess=nothing, data, dateFormat=nothing, csvDelimiter=nothing, returnContent=nothing,)
+#TODO: this format parameter allows odm, unlike most other format args
 	REDCap.request(
 		data=REDCap_data(data,REDCap_format(format),xml_tag="records"),
 		url=REDCap_url(url),
