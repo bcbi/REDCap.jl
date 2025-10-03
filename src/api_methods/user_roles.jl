9460933cd9f95e4543d73d5f2964e9ea3d34c174
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2025-10-03 15:07:41 -0400

    Improve docs format

--- src/api_methods/user_roles.jl
+++ src/api_methods/user_roles.jl
@@ -62,7 +62,7 @@ Import role assignments to a REDCap project
 # Named arguments
 - `url`: (read from `ENV["REDCAP_API_URL"]` by default)
 - `token`: an API token specific to the REDCap project and username (read from `ENV["REDCAP_API_TOKEN"]` by default)
-- `data`: May be a String, a file name, or a data type such as NamedTuple or Dict. Available attributes are user_name, unique_role_name, and data_access_group.
+- `data`: May be a String, a file name, or a data type such as NamedTuple or Dict. Available attributes are `user_name`, `unique_role_name`, and `data_access_group`.
 - `format`: the format of the `data` input parameter: `:csv`, `:json`, or `:xml` (default). If `data` is a String or a file name, this value must indicate the correct format. If `data` is a NamedTuple, Dict, or similar type, this value will determine what format will be used internally to pass on the data.
 - `returnFormat`: the desired output format: `:csv`, `:json`, or `:xml` (default)
 
@@ -84,7 +84,7 @@ Import roles to a REDCap project
 # Named arguments
 - `url`: (read from `ENV["REDCAP_API_URL"]` by default)
 - `token`: an API token specific to the REDCap project and username (read from `ENV["REDCAP_API_TOKEN"]` by default)
-- `data`: May be a String, a file name, or a data type such as NamedTuple or Dict. Available attributes are unique_role_name, role_label, design, alerts, user_rights, data_access_groups, reports, stats_and_charts, manage_survey_participants, calendar, data_import_tool, data_comparison_tool, logging, email_logging, file_repository, data_quality_create, data_quality_execute, api_export, api_import, api_modules, mobile_app, mobile_app_download_data, record_create, record_rename, record_delete, lock_records_customization, lock_records, lock_records_all_forms, forms, and forms_export.
+- `data`: May be a String, a file name, or a data type such as NamedTuple or Dict. Available attributes are `unique_role_name`, `role_label`, `design`, `alerts`, `user_rights`, `data_access_groups`, `reports`, `stats_and_charts`, `manage_survey_participants`, `calendar`, `data_import_tool`, `data_comparison_tool`, `logging`, `email_logging`, `file_repository`, `data_quality_create`, `data_quality_execute`, `api_export`, `api_import`, `api_modules`, `mobile_app`, `mobile_app_download_data`, `record_create`, `record_rename`, `record_delete`, `lock_records_customization`, `lock_records`, `lock_records_all_forms`, `forms`, and `forms_export`.
 - `format`: the format of the `data` input parameter: `:csv`, `:json`, or `:xml` (default). If `data` is a String or a file name, this value must indicate the correct format. If `data` is a NamedTuple, Dict, or similar type, this value will determine what format will be used internally to pass on the data.
 - `returnFormat`: the desired output format: `:csv`, `:json`, or `:xml` (default)
 
